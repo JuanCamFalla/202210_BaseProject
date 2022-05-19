a62f8228b49fe29ea3601bd6484628bfdf817353
JuanCFalla <j.fallag@uniandes.edu.co>
2022-05-19 14:48:41 -0500

    YAAAAA

--- src/app/bandas/banda.service.ts
+++ src/app/bandas/banda.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
-import { Banda } from './banda';
+import { Banda } from './bandas';
 import { BandaDetail } from './banda-detail';
 
 @Injectable({
